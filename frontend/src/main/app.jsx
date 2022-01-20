94a7747be12350459e1ab0213e9344c81c11c69d
galeno <gui.galenocover@gmail.com>
2022-01-20 15:51:24 -0300

    Components About and Todo was Created

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import "modules/bootstrap/dist/css/bootstrap.min.css"; //bootstrap -- CSS
 import "modules/font-awesome/css/font-awesome.min.css"; //font-awesome -- CSS
 
 import React from "react";
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 export default (props) => ( // Brackets is like an implicit "return"
   <div className="container">
-    <h1>test</h1>
+    <Todo/>
+    <About/>
   </div>
 );
