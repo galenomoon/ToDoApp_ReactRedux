615d54656c18b3b70ab7354bbc5554388fd501bb
galeno <gui.galenocover@gmail.com>
2022-01-20 17:44:48 -0300

    Event onChange

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,11 +5,27 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+
+    }
     render() {
         return (
             <div>
-                <PageHeader name='Tasks' small="Sig-in" />
-                <TodoForm />
+                <PageHeader name='Tasks' small="Sig-in" ></PageHeader>
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
