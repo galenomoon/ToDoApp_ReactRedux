6312d80aff32647b0048974c0db08815229467ff
galeno <gui.galenocover@gmail.com>
2022-01-20 14:11:54 -0300

    Enabling CORS

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,7 @@ module.exports = function(server){
     const router = express.Router();
     server.use('/api', router)
 
-    //TODO Routes
+    //TO-DO Routes
     const todoService = require("../api/todo/todoService");
     todoService.register(router, '/todos')
 }
--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003; //select port
 const bodyParser = require("body-parser"); //Parse of body of require
 const express = require("express"); //server web (on node)
 const server = express(); //starting express
+const allowCors = require("./cors");
 
 // all request => middleware
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 //register port
 server.listen(port, function () {
