0fff44023ad753cab458750d53b428973e8c6002
galeno <gui.galenocover@gmail.com>
2022-01-20 16:44:41 -0300

    PageHeader Component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,16 @@
 import React from "react"
+import PageHeader from '../template/pageHeader'
 
 export default props => (
 
     <div>
-        <h1>About</h1>
+      <PageHeader name='About' small="us" />
+      <h2>Our History</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Mission and Vision</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Media</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
     </div>
 
 
