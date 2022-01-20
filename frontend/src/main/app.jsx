bbcbd5199e4f3d18607f4b75c3b1aef6f4d860d1
galeno <gui.galenocover@gmail.com>
2022-01-20 16:37:10 -0300

    Settings | Routes (React-Router)

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,14 +2,12 @@ import "modules/bootstrap/dist/css/bootstrap.min.css"; //bootstrap -- CSS
 import "modules/font-awesome/css/font-awesome.min.css"; //font-awesome -- CSS
 
 import React from "react";
-import Todo from '../todo/todo'
-import About from '../about/about'
+import Routes from "./routes";
 import Menu from '../template/menu'
 
 export default (props) => ( // Brackets is like an implicit "return"
   <div className="container">
     <Menu />
-    <Todo />
-    <About />
+    <Routes />
   </div>
 );
