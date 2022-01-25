244e7fd0baa6bb4b9dc822308decabe4865c61ca
galeno <gui.galenocover@gmail.com>
2022-01-25 14:24:20 -0300

    Marking as Done/Pending Working with Action Creators

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 const URL = "http://localhost:3003/api/todos";
 
+//Action Creator - CHANGE STATE OF THE INPUT TITLE TASK CONTENT
 export const changeDescription = (event) => ({
   type: "DESCRIPTION_CHANGED",
   payload: event.target.value,
 });
 
+//Action Creator - SEARCH TASK
 export const search = () => {
   const request = axios.get(`${URL}?sort=-createdAt`);
   return {
@@ -15,16 +17,30 @@ export const search = () => {
   };
 };
 
-//Action Creator
-// export const addTask = (description) => {
-//   const request = axios.post(URL, { description });
-//   return [{ type: "TODO_ADDED", payload: request }, search()];
-// };
+//Action Creator - ADD TASK
 export const addTask = (description) => {
   return (dispatch) => {
     axios
-      .post(URL, { description })
-      .then((resp) => dispatch({ type: "TODO_ADDED", payload: resp.data }))
-      .then((resp) => dispatch(search()))
+    .post(URL, { description })
+    .then((resp) => dispatch({ type: "TODO_ADDED", payload: resp.data }))
+    .then((resp) => dispatch(search()));
+  };
+};
+
+//Action Creator - CHANGE STATUS OF THE TASK AS DONE 
+export const markAsDone = (todo) => {
+  return (dispatch) => {
+    axios
+      .put(`${URL}/${todo._id}`, { ...todo, done: true })
+      .then((resp) => dispatch(search()));
+  };
+};
+
+//Action Creator - CHANGE STATUS OF THE TASK AS PENDING
+export const markAsPending = (todo) => {
+  return (dispatch) => {
+    axios
+      .put(`${URL}/${todo._id}`, { ...todo, done: false })
+      .then((resp) => dispatch(search()));
   };
 };
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux"; //1) import connect
+import { bindActionCreators } from 'redux';
 import IconButton from "../template/iconButton";
-
+import { markAsDone, markAsPending } from "./todoActions";
 
 const TodoList = (props) => { //2) Transform the export default in a function
 
@@ -12,9 +13,9 @@ const TodoList = (props) => { //2) Transform the export default in a function
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)} />
+                        onClick={() => props.markAsDone(todo)} />
                     <IconButton style='warning' icon='undo' hide={!todo.done}
-                        onClick={() => props.handleMarkAsPending(todo)} />
+                        onClick={() => props.markAsPending(todo)} />
                     <IconButton style='danger' icon='trash-o' hide={!todo.done}
                         onClick={() => props.handleRemove(todo)} />
                 </td>
@@ -40,4 +41,6 @@ const TodoList = (props) => { //2) Transform the export default in a function
 const mapStateToProps = state => ({
     list: state.todo.list
 })
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = (dispatch) =>
+    bindActionCreators({ markAsDone, markAsPending }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
