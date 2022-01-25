dd7587daadd6e28b494febd42e5d93a25a1ba43c
galeno <gui.galenocover@gmail.com>
2022-01-25 13:29:11 -0300

    Adding Middlewares + BUG fix | Redux-Promise

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom"; //dom page
 import App from "./main/app";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import promise from 'redux-promise' //Middleware to finish incomplete promises 
 import { Provider } from 'react-redux';
 import reducers from './main/reducers';
 
 //EXTENSION GOOGLE CHROME - DEVTOOLS
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-&& window.__REDUX_DEVTOOLS_EXTENSION__()
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(reducers, devTools) //Application State
+const store = applyMiddleware(promise)(createStore)(reducers, devTools) //Application State
 
 ReactDOM.render(
     <Provider store={store}>
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,22 +1,6 @@
 const INITIAL_STATE = {
-  description: "Read a Book",
-  list: [
-    {
-      _id: 1,
-      description: "Subscribe in Guilherme Galeno's YouTube channel",
-      done: true,
-    },
-    {
-      _id: 2,
-      description: "Share Guilherme Galeno's YouTube channel",
-      done: false,
-    },
-    {
-      _id: 3,
-      description: "Listen Guilherme Galeno in Spotify",
-      done: false,
-    },
-  ],
+  description: " ",
+  list: [],
 };
 
 export default (state = INITIAL_STATE, action) => {
